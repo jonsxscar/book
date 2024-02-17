e4e6d9df04b0c2d408bae0ce3e62c643ab3600fb
jonsxscar <jonsxscar@gmail.com>
2024-02-17 18:57:17 -0500

    solucion bug filtro

--- resources/views/partials/book_list.blade.php
+++ resources/views/partials/book_list.blade.php
@@ -2,9 +2,9 @@
     <tr>
         <td>{{ $book->title }}</td>
         <td>{{ $book->author }}</td>
-        <td>
-            <a href="{{ route('reservations.create', ['id' => $book->id]) }}" class="btn btn-primary">Reservar</a>
-            <button class="btn btn-secondary show-more" data-description="{{ $book->description }}" data-image="{{ $book->image }}" data-category="{{ $book->category }}">Mostrar más</button>
-        </td>
+            <td>
+                <a href="{{ route('reservations.create', ['id' => $book->id]) }}" class="btn btn-primary">Reservar</a>
+                <button class="btn btn-secondary show-more" data-id="{{ $book->id }}" data-description="{{ $book->description }}" data-image="{{ $book->image }}" data-category="{{ $book->category }}">Mostrar más</button>
+            </td>
     </tr>
 @endforeach
